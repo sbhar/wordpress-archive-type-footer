913a3eed18afe3e4573dec3c012748740649e843
sugata bhar <sbhar123@gmail.com>
2016-04-12 12:43:28 +0530

    Removed Redundant Facebook plugin lines to make generic for everybody

--- footer.php
+++ footer.php
@@ -294,25 +294,6 @@
 
 <?php wp_footer(); ?>
 
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '719429244834636',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-	  //js.src = "//connect.facebook.net/en_US/sdk/debug.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
-
 </body>
 
 </html>
